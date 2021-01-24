ee2a2800a24f20bfbf379c5dfe46f6abe3c7faab
Nedim <nedimhodzic42@gmail.com>
2021-01-23 20:24:37 -0500

    working on home page

--- fbb-tracker/src/Components/Home.js
+++ fbb-tracker/src/Components/Home.js
@@ -3,9 +3,20 @@ import Button from "@material-ui/core/Button";
 import dimensions from "./dimensions";
 import "../CSS/HomeStyles.css";
 
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+
 function Home() {
   const [count, setCount] = useState(0);
   const { height, width } = dimensions();
+
+  const iconStyles = {
+    color: "#1e3046",
+    fontSize: "calc(1em + 1vmin)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
 
   return (
     <div className="parent">
@@ -16,6 +27,45 @@ function Home() {
       </div>
       <div className="right">
         <text className="titleTextRight">User Log In</text>
+        <form>
+          <div className="formContainer">
+            <label for="email">
+              <MailOutlineIcon style={iconStyles} />
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              className="inputStyle"
+              placeholder="Email"
+            />
+          </div>
+          <div className="formContainer" style={{ paddingTop: "5vh" }}>
+            <label for="password">
+              <LockOutlinedIcon style={iconStyles} />
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="inputStyle"
+              placeholder="Password"
+            />
+          </div>
+          <div className="checkboxContainer" style={{ paddingTop: "5vh" }}>
+            <div
+              type="checkbox"
+              id="remember"
+              name="remember"
+              className="checkboxStyle"
+              // placeholder="Password"
+            ></div>
+            <label for="remember" className="checkboxText">
+              Remember Me
+            </label>
+          </div>
+        </form>
+        <div className="placeholder"></div>
       </div>
     </div>
   );
